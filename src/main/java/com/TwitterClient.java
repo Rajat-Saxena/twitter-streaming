493f851b5cf50d3c302406b46f3c9ef816cc0da6
Rajat Saxena <rajatsaxena93@gmail.com>
2018-07-25 01:08:12 +0530

    Update TwitterClient

--- src/main/java/com/TwitterClient.java
+++ src/main/java/com/TwitterClient.java
@@ -2,26 +2,8 @@ package com;
 
 public class TwitterClient
 {
-<<<<<<< HEAD
     static String consumerKey = "";
     static String consumerSecret = "";
     static String accessToken = "";
     static String accessTokenSecret = "";
-=======
-    public static Client run(StatusesFilterEndpoint endpoint, BlockingQueue<String> queue)
-    {
-        String consumerKey = "";
-        String consumerSecret = "";
-        String token = "";
-        String secret = "";
-
-        Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
-        Client client = new ClientBuilder().hosts(Constants.STREAM_HOST)
-                .endpoint(endpoint)
-                .authentication(auth)
-                .processor(new StringDelimitedProcessor(queue)).build();
-
-        return client;
-    }
->>>>>>> dcd3c75cd87ea6352c4601b2fd7686965d20d33a
 }
