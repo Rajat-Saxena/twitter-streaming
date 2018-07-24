7a8a95b1aa75124f93c318b61c9d9472fe3d77d6
Rajat Saxena <rajatsaxena93@gmail.com>
2018-07-25 00:57:00 +0530

    Updated Producer and Consumer with Twitter4j streaming

--- src/main/java/com/TwitterClient.java
+++ src/main/java/com/TwitterClient.java
@@ -1,30 +1,10 @@
 package com;
 
-import com.twitter.hbc.ClientBuilder;
-import com.twitter.hbc.core.Client;
-import com.twitter.hbc.core.Constants;
-import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
-import com.twitter.hbc.core.processor.StringDelimitedProcessor;
-import com.twitter.hbc.httpclient.auth.Authentication;
-import com.twitter.hbc.httpclient.auth.OAuth1;
-
-import java.util.concurrent.BlockingQueue;
-
 public class TwitterClient
 {
-    public static Client run(StatusesFilterEndpoint endpoint, BlockingQueue<String> queue)
-    {
-        String consumerKey = "AcfTlGEf4Oo8z0toYrLvXxkvT";
-        String consumerSecret = "WrxCrKnJv36wPo0iQBgcO6s5BvIyi81GViKJ39r1QH5mar5ofG";
-        String token = "88861115-bIdAIbTr6fptyLQY4GPbRZDoKhN0eGbjU9pLL9EXw";
-        String secret = "l1MjJhrkStvtdnYh0tuTds5VszG7p3UfTGvEy5E6Rv7rk";
+    static String consumerKey = "";
+    static String consumerSecret = "";
+    static String accessToken = "";
+    static String accessTokenSecret = "";
 
-        Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
-        Client client = new ClientBuilder().hosts(Constants.STREAM_HOST)
-                .endpoint(endpoint)
-                .authentication(auth)
-                .processor(new StringDelimitedProcessor(queue)).build();
-
-        return client;
-    }
 }
--- src/main/java/com/TwitterProducer.java
+++ src/main/java/com/TwitterProducer.java
@@ -1,45 +1,93 @@
 package com;
 
-import com.google.common.collect.Lists;
-import com.twitter.hbc.core.Client;
-import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class TwitterProducer {
     public static void run() throws IOException
     {
-        String topicName = "dummyKafkaTopic";
+        String topicName = "twitter-streaming";
         InputStream input = null;
-        Producer producer;
+        Producer<String, String> producer = null;
+        TwitterStream twitterStream = null;
+        int tweetCounter = 0;
 
         try {
             Properties properties = new Properties();
-            input = new FileInputStream("src\\main\\resources\\producer.properties");
+            input = new FileInputStream("src/main/resources/producer.properties");
             properties.load(input);
 
             producer = new KafkaProducer<>(properties);
 
-            BlockingQueue<String> queue = new LinkedBlockingQueue<>(10000);
-            StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
-            endpoint.trackTerms(Lists.newArrayList("twitterapi", "#mufc"));
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true);
+            cb.setOAuthConsumerKey(TwitterClient.consumerKey);
+            cb.setOAuthConsumerSecret(TwitterClient.consumerSecret);
+            cb.setOAuthAccessToken(TwitterClient.accessToken);
+            cb.setOAuthAccessTokenSecret(TwitterClient.accessTokenSecret);
 
-            Client twitterClient = TwitterClient.run(endpoint, queue);
-            twitterClient.connect();
+            final LinkedBlockingQueue<Status> queue = new LinkedBlockingQueue<>(1000);
+            twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+            StatusListener listener = new StatusListener() {
+                @Override
+                public void onStatus(Status status) {
+                    queue.offer(status);
+                }
 
-            for (int tweets = 1; tweets < 1000; tweets++)
+                @Override
+                public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                    System.out.println("onDeletionNotice");
+                }
+
+                @Override
+                public void onTrackLimitationNotice(int i) {
+                    System.out.println("onTrackLimitationNotice");
+                }
+
+                @Override
+                public void onScrubGeo(long l, long l1) {
+                    System.out.println("onScrubGeo");
+                }
+
+                @Override
+                public void onStallWarning(StallWarning stallWarning) {
+                    System.out.println("onStallWarning");
+                }
+
+                @Override
+                public void onException(Exception e) {
+                    e.printStackTrace();
+                }
+            };
+
+            FilterQuery filterQuery = new FilterQuery();
+            String filterKeywords[] = {"MUFC", "mufc"};
+            filterQuery.track(filterKeywords);
+
+            twitterStream.addListener(listener);
+            twitterStream.filter(filterQuery);
+
+            while (tweetCounter <= 10)
             {
-                String message = queue.take();
-                System.out.println(message);
-                producer.send(new ProducerRecord<>(topicName, message ));
+                Status status = queue.poll();
+
+                if (status == null) {
+                    Thread.sleep(100);
+                }
+                else
+                {
+                    producer.send(new ProducerRecord<>(topicName, status.getText()));
+                    System.out.println("Tweet " + (++tweetCounter) + " sent.");
+                }
             }
         }
         catch (Exception e)
@@ -47,6 +95,8 @@ public class TwitterProducer {
             e.printStackTrace();
         }
         finally {
+            producer.close();
+            twitterStream.shutdown();
             input.close();
         }
     }
