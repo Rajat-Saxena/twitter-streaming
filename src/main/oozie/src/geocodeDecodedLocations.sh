6213a936ca73ce778bc3c376cf873706adfc7c04
Rajat Saxena <rajatsaxena93@gmail.com>
2018-08-12 17:51:45 +0530

    Adding stable location handler scripts

--- src/main/oozie/src/geocodeDecodedLocations.sh
+++ src/main/oozie/src/geocodeDecodedLocations.sh
@@ -4,6 +4,21 @@ echo "*********************************************"
 echo "INFO:  START geocodeDecodedLocations.sh"
 echo "*********************************************"
 
+NAME_NODE=hdfs://localhost:8020
+EXEC_DIR=/media/sf_Git-Repo/twitter-streaming/src/main/oozie/src  #${NAME_NODE}/projects/twitter-streaming/exec
+DECODED_FILE=${EXEC_DIR}/decoded_locations.txt
+GEOLOC_FILE=${EXEC_DIR}/geolocations.txt
+echo "INFO:  Exec Directory: ${EXEC_DIR} "
+
+cat ${DECODED_FILE} | while read location; do
+        location=${location// /"%20"}
+		echo "INFO:  Geocoding for $location "
+        result_json=`curl https://nominatim.openstreetmap.org/search/${location}?format=json&addressdetails=1&limit=1`
+        geoloc=`echo $result_json | jq '.[0]."lat", .[0]."lon"'`
+        geoloc=`echo $geoloc | sed 's/\n/ /g; s/\"//g; s/ /,/g'`
+        echo $geoloc >> ${GEOLOC_FILE}
+done 
+
 echo "*********************************************"
 echo "INFO:  END geocodeDecodedLocations.sh"
 echo "*********************************************"
--- src/main/oozie/src/getDecodedLocations.sh
+++ src/main/oozie/src/getDecodedLocations.sh
@@ -4,9 +4,20 @@ echo "*********************************************"
 echo "INFO:  START getDecodedLocations.sh"
 echo "*********************************************"
 
-echo "INFO:  Invoke spark-submit command"
-spark-submit
+EXEC_DIR=/media/sf_Git-Repo/twitter-streaming/src/main/oozie/src # ${NAME_NODE}/projects/twitter-streaming/exec
+DECODED_FILE=${EXEC_DIR}/decoded_locations.txt
+echo "INFO:  Exec Directory: ${EXEC_DIR} "
+
+for line in `cat ${EXEC_DIR}/encoded_locations.txt`; do
+        line=$(echo $line| sed 's/\"//g')
+        val=`echo -n $line | base64 -d`
+        echo $val >> ${DECODED_FILE}
+done
+
+echo "INFO:  Decoded locations saved to ${DECODED_FILE}"
 
 echo "*********************************************"
 echo "INFO:  END getDecodedLocations.sh"
 echo "*********************************************"
+
+sh geocodeDecodedLocations.sh
